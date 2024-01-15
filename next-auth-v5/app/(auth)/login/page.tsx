119908b147e2c7cfb711586892c588234a3837db
its-id <ik.dana24@gmail.com>
2024-01-15 20:59:56 +0530

    using ([folder name]) naming structure

---
+++ next-auth-v5/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+//using ([folder name]) does not adds to the route path. good for organizing our routes.
+const LoginPage = () => {
+  return (
+    <div>LoginPage without /auth/login</div>
+  )
+}
+
+export default LoginPage
--- next-auth-v5/app/dashboard/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DashboarPage = () => {
-  return (
-    <div>DashboarPage</div>
-  )
-}
-
-export default DashboarPage
