da6434e763d634f83d27b0323135a61e5b5fc16c
its-id <ik.dana24@gmail.com>
2024-01-15 21:11:19 +0530

    explaining server and client compoents

--- next-auth-v5/app/page.tsx
+++ next-auth-v5/app/page.tsx
@@ -1,6 +1,10 @@
+'use client'; //we use this keyword to make our page render on the client side. useful for using useState, useEffect hooks.
 import { Button } from '@/components/ui/button';
 
+
+// By default, all our pages our rendered as server components. useful for using all server related tasks, like fetching data from a database.
 export default function Home() {
+  console.log('WHERE AM I LOGGED?');
   return (
     <Button size='lg' variant='outline'>
       Click me!
